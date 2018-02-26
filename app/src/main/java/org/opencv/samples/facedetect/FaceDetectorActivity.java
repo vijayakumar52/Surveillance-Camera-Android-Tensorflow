35d5132f254d3c7d1d979a4534d6164a2b5b5175
Vijay <vijayakumar4057@gmail.com>
2018-02-26 21:35:35 +0530

    Pretrained model changed

--- app/src/main/java/org/opencv/samples/facedetect/FaceDetectorActivity.java
+++ app/src/main/java/org/opencv/samples/facedetect/FaceDetectorActivity.java
@@ -57,7 +57,7 @@ public class FaceDetectorActivity extends CameraActivity implements OnImageAvail
 
     private static final int TF_OD_API_INPUT_SIZE = 300;
     private static final String TF_OD_API_MODEL_FILE =
-            "file:///android_asset/ssd_mobilenet_v1_android_export.pb";
+            "file:///android_asset/frozen_inference_graph_face.pb";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco_labels_list.txt";
 
     // Configuration values for tiny-yolo-voc. Note that the graph is not included with TensorFlow and
